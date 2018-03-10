d321efdc3e3dc2733899c2cc677bc4d74d702031
Ripal Patel <patelripal.job@gmail.com>
2018-03-10 15:04:38 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using dotnetStandardClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operator o = new Operator();
+            var myList = o.CreateListOfPeople();
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dotnetStandardClassLibrary/Operator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetStandardClassLibrary
+{
+    public class Operator
+    {
+        public List<Person> CreateListOfPeople()
+        {
+            List<Person> output = new List<Person>();
+            output.Add(new Person {FirstName ="Ripal" , LastName = "Patel" });
+            output.Add(new Person {FirstName = "Tejas", LastName = "Patel" });
+            return output;
+        }
+    }
+}
